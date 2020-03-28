4950bdb4ceab5851021d94f53ad760b5084f8393
DELL <singh.chirag.rana@gmail.com>
2020-03-28 10:31:53 -0400

    made a constructor with 2 argument data and node

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -14,6 +14,11 @@ public class Node<E> {
         this.data = data;
     }
 
+    public Node(E data, Node<E> next) {
+        this.data = data;
+        this.next = next;
+    }
+
     public E getData() {
         return data;
     }
