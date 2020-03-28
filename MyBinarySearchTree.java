88ef1cff4de8d2ddc13f1343559acf8451dae8cb
chirag-singh-rana <59653508+chirag-singh-rana@users.noreply.github.com>
2020-03-28 09:36:08 -0400

    Add files via upload

---
+++ MyBinarySearchTree.java
@@ -0,0 +1,56 @@
+/*
+ *  Created by IntelliJ IDEA.
+ *  User: Vaibhav
+ *  Date: 23-Mar-20
+ *  Time: 7:17 PM
+ */
+package problem1.mybst;
+
+import problem1.node.TreeNode;
+
+// to implement BinarySearchTree
+public class MyBinarySearchTree<E> {
+    public int numberOfLeftChild = 0;
+    private TreeNode<E> root;
+    private int max = 0;
+
+    public TreeNode<E> getRoot() {
+        return root;
+    }
+
+    public boolean add(E data) {
+        return true;
+    }
+
+    private TreeNode<E> addRecursive(TreeNode<E> currentNode, E data) {
+        if (currentNode == null) {
+            return new TreeNode<E>(data);
+        } else if ((Integer) data < (Integer) currentNode.getData()) {
+            currentNode.setLeftChild(addRecursive(currentNode.getLeftChild(), data));
+        } else if ((Integer) data > (Integer) currentNode.getData()) {
+            currentNode.setRightChild((addRecursive(currentNode.getRightChild(), data)));
+        }
+        return currentNode;
+    }
+
+    private void printLeftChild(TreeNode<E> node, int level) {
+        if (node == null) {
+            return;
+        }
+        if (max < level) {
+            System.out.print(node.getData() + " ");
+        }
+        max = level;
+        if (node.getLeftChild() == null) {
+            numberOfLeftChild++;
+            max++;
+        }
+        printLeftChild(node.getLeftChild(), level + 1);
+        printLeftChild(node.getRightChild(), level + 1);
+    }
+
+    public void print() {
+        printLeftChild(root, 1);
+        System.out.println();
+    }
+}
