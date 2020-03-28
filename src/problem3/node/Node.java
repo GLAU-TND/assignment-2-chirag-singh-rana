35ad83e38073e9bee59a7b881b7b79e6ce36665b
DELL <singh.chirag.rana@gmail.com>
2020-03-28 10:25:47 -0400

    made getter and setter of data and next.

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -9,4 +9,20 @@ package problem3.node;
 public class Node<E> {
     private E data;
     private Node<E> next;
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
 }
