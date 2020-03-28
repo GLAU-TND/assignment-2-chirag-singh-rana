b8183c1f072f67828b5d3740aa3f33cc1d1e5873
DELL <singh.chirag.rana@gmail.com>
2020-03-28 08:52:38 -0400

    made getter and setter of datatypes.

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -10,4 +10,28 @@ public class TreeNode<E> {
     private E data;
     private TreeNode<E> leftChild;
     private TreeNode<E> rightChild;
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public TreeNode<E> getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(TreeNode<E> leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public TreeNode<E> getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(TreeNode<E> rightChild) {
+        this.rightChild = rightChild;
+    }
 }
