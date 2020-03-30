5fb6b88eb63e9d337d2a7affcbfd65096acd5e8c
DELL <singh.chirag.rana@gmail.com>
2020-03-30 12:52:35 -0400

    take a String input and split it and store in array.

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -5,7 +5,16 @@
  *  Time: 7:08 PM
  */
 package problem4.main;
+
+import java.util.Scanner;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER which are added in binarysearch tree");
+        String s = sc.nextLine();
+        String[] ar = s.split(" ");
+    }
 }
