0460bfccf8bdbd298057740f80f8aa8f742f6e86
DELL <singh.chirag.rana@gmail.com>
2020-03-30 13:06:23 -0400

    made a method traversePreOrder to print queue in pre order.

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -7,6 +7,7 @@
 package problem4.main;
 
 import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
 import problem4.myqueue.MyQueue;
 
 import java.util.Scanner;
@@ -25,11 +26,21 @@ public class MyMain {
         }
         MyQueue<Integer> myQueue = new MyQueue<>();
         System.out.println("nodes after traversing in pre order");
-        MyQueue<Integer> myQueue1 = new MyQueue<>();
+        MyQueue<Integer> myQueue1 = traversePreOrder(myBinarySearchTree.getRoot(), myQueue);
         System.out.println();
         System.out.println("Queue of pre order Successor :");
         MyQueue<Integer> myQueue2 = new MyQueue<>();
         System.out.println(myQueue2);
         MyQueue<Integer> myQueue3 = new MyQueue<>();
+    }
+
+    public static MyQueue traversePreOrder(TreeNode<Integer> currentNode, MyQueue<Integer> myQueue) {
+        if (currentNode != null) {
+            myQueue.enqueue(currentNode.getData());
+            System.out.println(currentNode.getData());
+            traversePreOrder(currentNode.getLeftChild(), myQueue);
+            traversePreOrder(currentNode.getRightChild(), myQueue);
+        }
+        return myQueue;
     }
 }
